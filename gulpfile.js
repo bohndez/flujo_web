37b17407d0bd936ac3f539645bd926532a758c12
bohndez <globaldev@No.local>
2016-03-31 12:27:50 -0300

    Ahora con LESS y SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,86 @@
 /*
 * Para trabajar con gulp.
 * Boris Hernández.
-* http://www.cajonarioum.cl
+* https://github.com/bohndez
 */
 
-//Dependencias - - -
-var gulp 		= require('gulp'),
-	// cambio		= require('gulp-changed'),
-	concat 		= require('gulp-concat'),
-	uglify 		= require('gulp-uglify'),
-	less 		= require('gulp-less'),
-	minifyCSS 	= require('gulp-minify-css'),
-	notify 		= require('gulp-notify'),
-	livereload 	= require('gulp-livereload'),
-	// imagemin 	= require('gulp-imagemin'),
-	// cache 		= require('gulp-cache'),
-	// jshint 		= require('gulp-jshint'),
-	rename 		= require('gulp-rename');
+// DEPENDENCIAS
+// ------------
+var gulp 		= require('gulp'),              // Automatiza las tareas.
+	concat 		= require('gulp-concat'),       // Toma el contenido de varios archivos y los deja en un único archivo.
+	uglify 		= require('gulp-uglify'),       // Comprime el código Javasscript.
+	less 		= require('gulp-less'),         // Procesa archivos ".less" y los deja como  ".css".
+	sass 		= require('gulp-sass'),         // Procesa archivos ".scss" y los deja como  ".css".
+	cleanCss 	= require('gulp-clean-css'),    // Minifica los archivos CSS.
+	notify 		= require('gulp-notify'),		// Lanza notificaciones del sistema operativo.
+	livereload 	= require('gulp-livereload'),   // Detecta si hubo cambios en los archivos y refresca el navegador si los hay.
+	imagemin 	= require('gulp-imagemin'),     // Comprime imágenes para dejarlas más livianas.
+	rename 		= require('gulp-rename');       // Cambia nombres y agrega prefijos y sufijos.
 
-//Tareas - - - - - -
-gulp.task('less', function () {
-	return gulp.src('src/less/*.less')
-	.pipe(less())
-	.on("error", notify.onError("<%= error.message %>"))
-	.pipe(minifyCSS())
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('dist/assets/css/'))
-	.pipe(notify({ message: 'less listo!'}));
-});
+// TAREAS
+// ------
 
-gulp.task('js', function () {
-	return gulp.src('src/js/*.js')
-	.pipe(uglify())
-	.on("error", notify.onError("<%= error.message %>"))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('dist/assets/js/'))
-	.pipe(notify({ message: 'js listo!'}));
-});
+	// Procesa y comprime los archivos LESS. - - -
+	gulp.task('less', function () {
+		return gulp.src('src/less/*.less')
+		.pipe(less())
+		.on("error", notify.onError("<%= error.message %>"))
+		.pipe(cleanCss())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/assets/css/'))
+		.pipe(notify({ message: 'Less listo!'}));
+	});
 
-gulp.task('libs', function () {
-	return gulp.src('src/js/libs/*.js')
-	.pipe(concat('libs.min.js'))
-	.pipe(uglify())
-	.on("error", notify.onError("<%= error.message %>"))
-	.pipe(gulp.dest('dist/assets/js/libs/'))
-});
+	// Procesa y comprime los archivos SASS. - - -
+	gulp.task('sass', function () {
+		return gulp.src('src/sass/*.scss')
+		.pipe(less())
+		.on("error", notify.onError("<%= error.message %>"))
+		.pipe(cleanCss())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/assets/css/'))
+		.pipe(notify({ message: 'Sass listo!'}));
+	});
 
-// gulp.task('images', function() {
-// 	return gulp.src('src/images/**/*')
-// 	.pipe(cambio('dist/assets/images/'))
-// 	.pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-// 	.pipe(gulp.dest('dist/assets/images/'))
-// 	.pipe(notify({ message: 'Imágenes comprimidas!' }));
-// });
+	// Minifica los archivos Javascript. - - -
+	gulp.task('js', function () {
+		return gulp.src('src/js/*.js')
+		.pipe(uglify())
+		.on("error", notify.onError("<%= error.message %>"))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/assets/js/'))
+		.pipe(notify({ message: 'JS listo!'}));
+	});
 
-// gulp.task('jshint', function() {
-// 	return gulp.src('src/js/main.js')
-// 	.pipe(jshint());
-// });
+	// Minifica y deja en un único archivo los archivos Javascript que estén en "src/js/libs/". - - -
+	gulp.task('libs', function () {
+		return gulp.src('src/js/libs/*.js')
+		.pipe(concat('libs.min.js'))
+		.pipe(uglify())
+		.on("error", notify.onError("<%= error.message %>"))
+		.pipe(gulp.dest('dist/assets/js/libs/'))
+	});
 
-gulp.task('watch', function() {
-	gulp.watch('src/less/*.less', ['less']);
-	gulp.watch('src/js/*.js', ['js']);
-	gulp.watch('src/js/libs/*.jss', ['libs']);
-	gulp.watch('src/images/**/*', ['images']);
+	// Comprime las imágenes.
+	gulp.task('images', function() {
+		return gulp.src('src/images/**/*')
+		.pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
+		.pipe(gulp.dest('dist/assets/images/'))
+		.pipe(notify({ message: 'Imágenes comprimidas!' }));
+	});
 
-	livereload.listen();
-	gulp.watch(['dist/**']).on('change', livereload.changed);
-});
+	// Monitorea si hay cambios. Si los hay, ejecuta la tarea correspondiente. - - -
+	gulp.task('watch', function() {
+		gulp.watch('src/less/*.less', ['less']);
+		gulp.watch('src/sass/*.scss', ['sass']);
+		gulp.watch('src/js/*.js', ['js']);
+		gulp.watch('src/js/libs/*.js', ['libs']);
+		gulp.watch('src/images/**/*', ['images']);
 
-gulp.task('default', ['less', 'js', 'libs', 'watch']);
+		livereload.listen(); // Permite ver si hay cambios y refresca de inmediato el navegador si los hay.
+		gulp.watch(['dist/**']).on('change', livereload.changed);
+	});
+
+// LLAMADO A LAS TAREAS
+// --------------------
+gulp.task('default', ['less', 'sass', 'js', 'libs', 'images', 'watch']);
